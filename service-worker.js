a6a3bf6fb93a11f4246a514e2fb33d49aff38f35
SolodkayaT <tatasladkaya@gmail.com>
2020-03-11 12:28:09 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-05-phonebook-ctx/precache-manifest.44947a5dd5313ae700e23219c909fedd.js"
+  "/goit-react-hw-05-phonebook-ctx/precache-manifest.cbb9732d48942eec20a5d3d0dc0291e8.js"
 );
 
 self.addEventListener('message', (event) => {
